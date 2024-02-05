b7f9cbc88ee5b0c163784bee117da758b877e65f
SUMAIYYAANJUM <kunnisa826@gmail.com>
2024-02-05 16:04:17 +0530

    made database/leave_employee.sql

---
+++ database/leave_employee.sql
@@ -0,0 +1,46 @@
+--
+-- Database: `employee_leave`
+--
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `UserName` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbldepartments`
+--
+
+INSERT INTO `tbldepartments` (`id`, `DepartmentName`, `DepartmentShortName`, `CreationDate`) VALUES
+(2, 'Information Technologies', 'ICT', '2017-11-01 07:19:37'),
+(3, 'Library', 'LIb', '2021-05-21 08:27:45');
+
+-- --------------------------------------------------------
+
+
+--
+-- Table structure for table `tbldepartments`
+--
+
+CREATE TABLE `tbldepartments` (
+  `id` int(11) NOT NULL,
+  `DepartmentName` varchar(150) DEFAULT NULL,
+  `DepartmentShortName` varchar(100) NOT NULL,
+  `CreationDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbldepartments`
+--
+
+INSERT INTO `tbldepartments` (`id`, `DepartmentName`, `DepartmentShortName`, `CreationDate`) VALUES
+(2, 'Information Technologies', 'ICT', '2017-11-01 07:19:37'),
+(3, 'Library', 'LIb', '2021-05-21 08:27:45');
+
+-- --------------------------------------------------------
+
