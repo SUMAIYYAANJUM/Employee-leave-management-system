2b5ff9a086d3ecd5aa1815d3aedadf14857a1f55
SUMAIYYAANJUM <kunnisa826@gmail.com>
2024-02-09 15:56:22 +0530

    table modified

--- database/leave_employee.sql
+++ database/leave_employee.sql
@@ -1,5 +1,5 @@
 --
--- Database: `employee_leave`
+-- Database: `employee_leave_managementt`
 --
 --
 -- Table structure for table `admin`
@@ -9,19 +9,18 @@ CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
   `UserName` varchar(100) NOT NULL,
   `Password` varchar(100) NOT NULL,
-  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
+  `updationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tbldepartments`
+-- Dumping data for table `admin`
 --
 
-INSERT INTO `tbldepartments` (`id`, `DepartmentName`, `DepartmentShortName`, `CreationDate`) VALUES
-(2, 'Information Technologies', 'ICT', '2017-11-01 07:19:37'),
-(3, 'Library', 'LIb', '2021-05-21 08:27:45');
+INSERT INTO `admin` (`id`, `UserName`, `Password`, `updationDate`) VALUES
+(1, 'admin', 'admincse@123', '2023-11-03 05:55:30');
 
 -- --------------------------------------------------------
-
 
 --
 -- Table structure for table `tbldepartments`
@@ -39,8 +38,8 @@ CREATE TABLE `tbldepartments` (
 --
 
 INSERT INTO `tbldepartments` (`id`, `DepartmentName`, `DepartmentShortName`, `CreationDate`) VALUES
-(2, 'Information Technologies', 'ICT', '2017-11-01 07:19:37'),
-(3, 'Library', 'LIb', '2021-05-21 08:27:45');
+(1, 'Information Technologies', 'IT', '2019-11-01 07:19:37'),
+(2, 'Computer Science', 'CSE', '2020-05-21 08:27:45');
 
 -- --------------------------------------------------------
 
@@ -71,11 +70,77 @@ CREATE TABLE `tblemployees` (
 --
 
 INSERT INTO `tblemployees` (`emp_id`, `FirstName`, `LastName`, `EmailId`, `Password`, `Gender`, `Dob`, `Department`, `Address`, `Av_leave`, `Phonenumber`, `Status`, `RegDate`, `role`, `location`) VALUES
-(1, 'Janobe', 'Martins', 'janobe@janobe.com', '36d59e2369f00c4d9f336acf4408bae9', 'Male', '3 February, 1990', 'ICT', 'N NEPO', '30', '0248865955', 1, '2017-11-10 11:29:59', 'Staff', 'NO-IMAGE-AVAILABLE.jpg'),
-(2, 'Edem', 'Mcwilliams', 'james@gmail.com', 'b4cc344d25a2efe540adbf2678e2304c', 'Male', '3 February, 1990', 'ICT', 'N NEPO', '30', '8587944255', 1, '2017-11-10 13:40:02', 'Admin', 'photo2.jpg'),
-(4, 'Nathaniel', 'Nkrumah', 'nat@gmail.com', 'b4cc344d25a2efe540adbf2678e2304c', 'Male', '3 February, 1990', 'ICT', 'N NEPO', '30', '587944255', 1, '2017-11-10 13:40:02', 'Admin', 'NO-IMAGE-AVAILABLE.jpg'),
-(5, 'Gideon', 'Annan', 'gideon@gmail.com', 'b4cc344d25a2efe540adbf2678e2304c', 'Male', '3 February, 1990', 'ICT', 'N NEPO', '30', '587944255', 1, '2017-11-10 13:40:02', 'HOD', 'photo5.jpg'),
-(6, 'Martha', 'Arthur', 'mat@gmail.com', 'b4cc344d25a2efe540adbf2678e2304c', 'Female', '3 February, 1990', 'LIb', 'N NEPO', '30', '587944255', 1, '2017-11-10 13:40:02', 'Staff', 'NO-IMAGE-AVAILABLE.jpg'),
-(7, 'Bridget', 'Gafa', 'bridget@gmail.com', 'b4cc344d25a2efe540adbf2678e2304c', 'Female', '3 February, 1990', 'ICT', 'N NEPO', '1', '0596667981', 1, '2017-11-10 13:40:02', 'Staff', '1920_File_logo4.png'),
-(8, 'Anna', 'Mensah', 'an@gmail.com', 'b4cc344d25a2efe540adbf2678e2304c', 'Female', '3 February, 1990', 'ICT', 'N NEPO', '30', '587944255', 1, '2017-11-10 13:40:02', 'HOD', 'NO-IMAGE-AVAILABLE.jpg');
+(1, 'Imtiyaz', 'Hassan', 'imtiyaz@hassan.com', 'imtiyaz@123', 'Male', '3 February, 1890', 'IT', 'HYD', '30', '0248865955', 1, '2020-11-10 11:29:59', 'HOD', 'NO-IMAGE-AVAILABLE.jpg'),
+(2, 'Pradeep', 'Kumar', 'pradeep@gmail.com', 'pradeep@123', 'Male', '3 February, 1890', 'IT', 'HYD', '30', '8587944255', 1, '2020-11-10 11:29:59', 'HOD', 'photo2.jpg'),
+(3, 'admin', 'admin', 'admin@gmail.com', 'admin@123', 'Male', '3 February, 1990', 'IT', 'HYD', '30', '587944255', 1, '2020-11-10 11:29:59', 'Admin', 'NO-IMAGE-AVAILABLE.jpg'),
+(4, 'Ulafth', 'Banu', 'ulfath@gmail.com', 'ulfath@123', 'Female', '3 February, 1990', 'CSE', 'HYD', '30', '587944255', 1, '2020-11-10 11:29:59', 'staff', 'photo5.jpg'),
+(5, 'Kareem', 'Unnisa', 'kareema@gmail.com', 'kareema@2003', 'Female', '29 August, 2003', 'CSE', 'KNR', '30', '587944255', 1, '2020-11-10 13:40:02', 'Staff', 'NO-IMAGE-AVAILABLE.jpg'),
+(6, 'Kishwar', 'Afsana', 'kishwar@gmail.com', 'kishwar@123', 'Female', '3 February, 2000', 'IT', 'Bihar', '1', '0596667981', 1, '2020-11-10 13:40:02', 'Staff', '1920_File_logo4.png'),
+(7, 'skrma', 'nisasha', 'skrma@gmail.com', 'nisasha@2021', 'Female', '3 February, 1990', 'IT', 'Bihar', '30', '587944255', 1, '2021-11-10 13:40:02', 'staff', 'NO-IMAGE-AVAILABLE.jpg');
 
+-- --------------------------------------------------------
+
+--
+--
+-- Table structure for table `tblleaves`
+--
+
+CREATE TABLE `tblleaves` (
+  `id` int(11) NOT NULL,
+  `LeaveType` varchar(110) NOT NULL,
+  `ToDate` varchar(120) NOT NULL,
+  `FromDate` varchar(120) NOT NULL,
+  `Description` mediumtext NOT NULL,
+  `PostingDate` date NOT NULL,
+  `AdminRemark` mediumtext,
+  `registra_remarks` mediumtext NOT NULL,
+  `AdminRemarkDate` varchar(120) DEFAULT NULL,
+  `Status` int(1) NOT NULL,
+  `admin_status` int(11) NOT NULL DEFAULT '0',
+  `IsRead` int(1) NOT NULL,
+  `empid` int(11) DEFAULT NULL,
+  `num_days` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+
+--
+-- Dumping data for table `tblleaves`
+--
+
+INSERT INTO `tblleaves` (`id`, `LeaveType`, `ToDate`, `FromDate`, `Description`, `PostingDate`, `AdminRemark`, `registra_remarks`, `AdminRemarkDate`, `Status`, `admin_status`, `IsRead`, `empid`, `num_days`) VALUES
+(1, 'Casual Leave', '2023-05-02', '2023-05-12', 'I want to take a leave.', '2023-05-20', 'Ok', 'ok', '2023-05-24 20:26:19 ', 1, 1, 1, 7, 3),
+(2, 'Medical Leave', '08-05-2023', '11-05-2023', 'Noted', '0000-00-00', 'Not this time', '', '2023-05-21 0:31:10 ', 0, 0, 1, 6, 4),
+(3, 'Casual Leave', '02-05-2023', '05-05-2023', 'Nice Leave', '2023-05-20', 'Ok', 'Noted', '2023-05-24 20:42:18 ', 1, 1, 1, 7, 4),
+(4, 'Casual Leave', '11-05-2023', '15-05-2023', 'Just', '2021-05-23', 'Leave Approved', 'Noted', '2023-05-24 19:56:45 ', 1, 1, 1, 7, 5);
+
+-- --------------------------------------------------------
+
+---
+--- Table structure for table `tblleavetype`
+--
+
+CREATE TABLE `tblleavetype` (
+  `id` int(11) NOT NULL,
+  `LeaveType` varchar(200) DEFAULT NULL,
+  `Description` mediumtext,
+  `date_from` varchar(200) NOT NULL,
+  `date_to` varchar(200) NOT NULL,
+  `CreationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+--
+
+-- Dumping data for table `tblleavetype`
+--
+
+INSERT INTO `tblleavetype` (`id`, `LeaveType`, `Description`, `date_from`, `date_to`, `CreationDate`) VALUES
+(1, 'Casual Leave', 'Casual Leave', '2023-05-23', '2023-06-20', '2023-05-19 14:32:03'),
+(2, 'Medical Leave', 'Medical Leave', '2023-05-05', '2023-05-28', '2023-05-19 15:29:05'),
+(3, 'Other', 'Leave all staff', '31-05-2023', '04-06-2023', '2023-05-20 17:17:43');
+
+-- --------------------------------------------------------
+
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
